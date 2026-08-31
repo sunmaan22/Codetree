473689b3323d17bf3602a6ddc1d0c7fabd4f01dc
SUNMA4N <sunmaan22@gmail.com>
2026-08-31 14:44:01 +0900

    [Passed] 음수 구별하기 (2ms, 0MB)

---
+++ trail1/음수 구별하기/separate-negative-number.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a;
+    cin >> a;
+    cout << a << endl;
+    if(a<0){
+        cout<<"minus";
+    }
+    // Please write your code here.
+    return 0;
+}
