17cbf4dc71c2380c104724d7c1fdd47decc5757f
SUNMA4N <sunmaan22@gmail.com>
2026-08-31 21:55:01 +0900

    [Passed] 출석 부르기 (2ms, 0MB)

---
+++ trail1/출석 부르기/calling-attendance.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a;
+    cin >> a;
+
+    if(a==1){
+        cout << "John";
+    }
+    else if(a==2){
+        cout<<"Tom";
+    }
+    else if(a==3){
+        cout<<"Paul";
+    }
+    else{
+        cout << "Vacancy";
+    }
+    // Please write your code here.
+    return 0;
+}
