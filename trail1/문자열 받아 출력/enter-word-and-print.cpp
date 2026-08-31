7d44ad5f3fd713c24d7997bfd4a578098b4283c2
SUNMA4N <sunmaan22@gmail.com>
2026-08-31 13:41:31 +0900

    [Passed] 문자열 받아 출력 (2ms, 0MB)

---
+++ trail1/문자열 받아 출력/enter-word-and-print.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    string a;
+    cin >> a;
+    cout <<a ;
+    
+    // Please write your code here.
+    return 0;
+}
