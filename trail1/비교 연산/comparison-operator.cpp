6e5beadf38633ce83558357ea3cbafd118feeb40
SUNMA4N <sunmaan22@gmail.com>
2026-08-31 22:18:28 +0900

    [Passed] 비교 연산 (2ms, 0MB)

---
+++ trail1/비교 연산/comparison-operator.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a,b;
+    cin>>a>>b;
+    cout<<(a>=b)<<endl<<(a>b)<<endl<<(b>=a)<<endl<<(b>a)<<endl<<(a==b)<<endl<<(a!=b);
+    // Please write your code here.
+    return 0;
+}
