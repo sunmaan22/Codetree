04277d82256bbe14483b35c9a98409b6fe145e49
SUNMA4N <sunmaan22@gmail.com>
2026-08-31 10:49:26 +0900

    [Passed] 변수 값 교체하기 5 (1ms, 0MB)

---
+++ trail1/변수 값 교체하기 5/replacing-variable-values-5.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a = 2;
+    int b = 6;
+    a=b;
+
+    cout << a << endl << b;
+    // Please write your code here.
+    return 0;
+}
