873d5de81cc038b87ee07cd91bb1b5b9523dbbe8
SUNMA4N <sunmaan22@gmail.com>
2026-08-31 22:22:44 +0900

    [Passed] 4번의 크기 비교 (2ms, 0MB)

---
+++ trail1/4번의 크기 비교/4-time-comparison.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a,b,c,d,e;
+    cin>>a>>b>>c>>d>>e;
+
+    cout<<(a>b)<<endl<<(a>c)<<endl<<(a>d)<<endl<<(a>e);
+    // Please write your code here.
+    return 0;
+}
