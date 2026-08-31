d3f96b5adddc774ddb737bec608a7842887ee26e
SUNMA4N <sunmaan22@gmail.com>
2026-08-31 21:45:19 +0900

    [Passed] 삼항연산자 (2ms, 0MB)

---
+++ trail1/삼항연산자/ternary-operator.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    int a;
+    string c;
+    cin >> a ;
+
+    c = a == 100 ? "pass" : "failure";
+    cout << c;
+    // Please write your code here.
+    return 0;
+}
