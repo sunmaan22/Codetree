8e889975727323ff5d9739a321c13596242fa1f0
SUNMA4N <sunmaan22@gmail.com>
2026-08-31 22:06:42 +0900

    [Passed] 정수의 조건 여부 2 (2ms, 0MB)

---
+++ trail1/정수의 조건 여부 2/numbers-condition-2.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a;
+    cin >> a;
+
+    if(a==5){
+        cout << "A";
+    }
+    else if(a%2==0){
+        cout << "B";
+    }
+    else{
+
+    }
+    // Please write your code here.
+    return 0;
+}
