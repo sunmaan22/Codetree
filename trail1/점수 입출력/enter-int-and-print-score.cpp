a5664ee6ba4b3c4719a68ed56d821236f72709e1
SUNMA4N <sunmaan22@gmail.com>
2026-08-31 11:11:51 +0900

    [Passed] 점수 입출력 (2ms, 0MB)

---
+++ trail1/점수 입출력/enter-int-and-print-score.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a;
+    cin >>  a;
+    cout << "Your score is "<<a<<" point.";
+    // Please write your code here.
+    return 0;
+}
