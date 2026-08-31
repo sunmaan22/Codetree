64ee1c468eba0cc8f194312aabf2d9a28402aefe
SUNMA4N <sunmaan22@gmail.com>
2026-08-31 11:08:06 +0900

    [Passed] 변수 값 복사하기 2 (2ms, 0MB)

---
+++ trail1/변수 값 복사하기 2/copying-variable-values-2.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a = 5 , b = 6 , c = 7;
+
+    a=b=c;
+
+    cout << a << " "  << b << " "<<c;
+    // Please write your code here.
+    return 0;
+}
