8e701801f4a8d3a9df19fda39100d9dc26bdcc9c
SUNMA4N <sunmaan22@gmail.com>
2026-08-31 14:11:25 +0900

    [Passed] 입력받아 연산하기 (2ms, 0MB)

---
+++ trail1/입력받아 연산하기/take-input-and-operate.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a ,b;
+    cin >> a >> b;
+    a += 87;
+    b %= 10;
+
+    cout << a <<endl << b;
+    // Please write your code here.
+    return 0;
+}
