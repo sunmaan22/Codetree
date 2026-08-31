0d27831ca9b15093aff70ec7caed75a13ca7186e
SUNMA4N <sunmaan22@gmail.com>
2026-08-31 22:11:40 +0900

    [Passed] 두 수의 짝홀 여부 (1ms, 0MB)

---
+++ trail1/두 수의 짝홀 여부/parity-of-two-numbers.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a,b;
+    cin >> a >>b;
+
+    if(a%2 == 0){
+        cout << "even"<<endl;
+    }
+    else{
+        cout << "odd"<<endl;
+    }
+    if(b%2 == 0){
+        cout <<"even";
+
+    }
+    else{
+        cout << "odd";
+    }
+    // Please write your code here.
+    return 0;
+}
