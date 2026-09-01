0d816d45bf284971ef9b064b7890d171597ccc77
SUNMA4N <sunmaan22@gmail.com>
2026-09-01 13:19:40 +0900

    [Passed] 문자열 출력하기 (2ms, 0MB)

---
+++ trail1/문자열 출력하기/print-string.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a;
+    cin >> a;
+
+    for(int i=0;i<a;i++){
+        cout << "LeebrosCode" <<endl;
+    }
+    // Please write your code here.
+    return 0;
+}
