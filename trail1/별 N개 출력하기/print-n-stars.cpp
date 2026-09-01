a32aa671416ed3ab2cb4c326316e8b8700261fa5
SUNMA4N <sunmaan22@gmail.com>
2026-09-01 13:04:42 +0900

    [Passed] 별 N개 출력하기 (3ms, 0MB)

---
+++ trail1/별 N개 출력하기/print-n-stars.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a;
+    cin >> a;
+
+    int i = 1;
+    while(i<=a){
+        cout << "*" <<endl;
+        i++;
+    }
+    // Please write your code here.
+    return 0;
+}
