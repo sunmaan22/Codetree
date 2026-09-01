c070947b1b5f1181a212075fc527ea5a64cd115e
SUNMA4N <sunmaan22@gmail.com>
2026-09-01 13:03:39 +0900

    [Passed] N까지 자연수 출력 (2ms, 0MB)

---
+++ trail1/N까지 자연수 출력/print-1-to-n.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a;
+    cin >> a;
+
+    int i = 1;
+
+    while(i<=a){
+        cout << i << " ";
+        i++;
+    }
+    // Please write your code here.
+    return 0;
+}
