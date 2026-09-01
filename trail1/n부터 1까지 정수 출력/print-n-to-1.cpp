894bdc3f7d8207bccd386e191ecfe57571660185
SUNMA4N <sunmaan22@gmail.com>
2026-09-01 13:15:14 +0900

    [Passed] n부터 1까지 정수 출력 (2ms, 0MB)

---
+++ trail1/n부터 1까지 정수 출력/print-n-to-1.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+
+    while(n>=1){
+        cout << n << " ";
+        n--;
+    }
+    // Please write your code here.
+    return 0;
+}
