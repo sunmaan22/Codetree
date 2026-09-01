67885aac578d96a922c4cb88ca4ae91e29486efe
SUNMA4N <sunmaan22@gmail.com>
2026-09-01 12:51:16 +0900

    [Passed] 정수 입력받아 배수 출력 (2ms, 0MB)

---
+++ trail1/정수 입력받아 배수 출력/print-multiple-of-input.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+
+    for(int i =1;i<=5;i++){
+        cout << i*n << " ";
+    }
+    // Please write your code here.
+    return 0;
+}
