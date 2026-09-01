d1f0a6addb500511f8bdc95219b3362afbfe7480
SUNMA4N <sunmaan22@gmail.com>
2026-09-01 11:08:34 +0900

    [Passed] 최댓값 구하기 (2ms, 0MB)

---
+++ trail1/최댓값 구하기/maximum-value.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a,b,c,max;
+    cin >> a >> b >> c;
+    if(a>=b){
+        max=a;
+    }
+    else{
+        max = b;
+    }
+    if(max<=c){
+        max = c;
+    }
+    cout << max;
+    // Please write your code here.
+    return 0;
+}
