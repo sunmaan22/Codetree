28ed2db0c2c4cadc8bd41a722f5734c87b81716e
SUNMA4N <sunmaan22@gmail.com>
2026-09-01 10:10:31 +0900

    [Passed] 둘 중 하나의 배수 (2ms, 0MB)

---
+++ trail1/둘 중 하나의 배수/multiple-of-either.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a;
+    cin >> a;
+    if(a%3==0||a%5==0){
+        cout << 1;
+    }
+    else{
+        cout << 0;
+    }
+    // Please write your code here.
+    return 0;
+}
