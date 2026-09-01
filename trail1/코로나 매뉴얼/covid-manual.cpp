5d43b045071e073dcc7eaeeebdcabbc0f0981601
SUNMA4N <sunmaan22@gmail.com>
2026-09-01 11:22:27 +0900

    [Passed] 코로나 매뉴얼 (2ms, 0MB)

--- trail1/코로나 매뉴얼/covid-manual.cpp
+++ trail1/코로나 매뉴얼/covid-manual.cpp
@@ -49,3 +49,43 @@ int main() {
     // Please write your code here.
     return 0;
 }
+
+/*
+#include <iostream>
+using namespace std;
+
+// 작성하셨던 is_emergency 함수
+char is_emergency(char a, int b) {
+    if (a == 'Y' && b >= 37) return 'A';
+    else if (a == 'N' && b >= 37) return 'B';
+    else if (a == 'Y' && b < 37) return 'C';
+    else return 'D';
+}
+
+int main() {
+    int count = 0;
+
+    // 3번 반복하는 for문
+    for (int i = 0; i < 3; i++) {
+        char symptom;
+        int temp;
+        
+        // 한 명씩 차례대로 입력받음
+        cin >> symptom >> temp;
+        
+        // 입력받자마자 바로 A인지 확인하고 count 증가
+        if (is_emergency(symptom, temp) == 'A') {
+            count++;
+        }
+    }
+
+    // 이후 count 값에 따라 E 또는 N 출력 (문제 조건에 맞게)
+    if (count >= 2) {
+        cout << "E";
+    } else {
+        cout << "N";
+    }
+
+    return 0;
+}
+*/
