3452090fb3a3e62d03f78ec3cd4b978d9376b36f
SUNMA4N <sunmaan22@gmail.com>
2026-09-01 09:45:36 +0900

    [Passed] 특정 조건 세 정수 비교 (2ms, 0MB)

---
+++ trail1/특정 조건 세 정수 비교/specific-comparison-of-three-natural-numbers.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int a,b,c;
+    cin >> a >> b >> c;
+    if(a<=b&&a<=c){
+        cout << 1 << " ";
+    }
+    else{
+        cout << 0 << " ";
+    }
+    if(a==b&&a==c){
+        cout << 1;
+    }
+    else{
+        cout <<0;
+    }
+    // Please write your code here.
+    return 0;
+}
