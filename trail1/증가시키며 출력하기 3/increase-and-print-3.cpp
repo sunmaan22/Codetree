925851b40bd4d7a1b859eab8823c28eaf8e15a2c
SUNMA4N <sunmaan22@gmail.com>
2026-09-01 13:05:41 +0900

    [Passed] 증가시키며 출력하기 3 (2ms, 0MB)

---
+++ trail1/증가시키며 출력하기 3/increase-and-print-3.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int i = 10;
+    while(i<=26){
+        cout << i << " ";
+        i += 2;
+    }
+    // Please write your code here.
+    return 0;
+}
