be9d7a96486827fac336322e74a4a1b23e52e250
SUNMA4N <sunmaan22@gmail.com>
2026-09-01 13:18:40 +0900

    [Passed] 문자 출력하기 (2ms, 0MB)

---
+++ trail1/문자 출력하기/print-text.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    char a;
+    cin >> a;
+
+    for(int i=0;i<8;i++){
+        cout << a;
+    }
+    // Please write your code here.
+    return 0;
+}
