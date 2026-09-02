5be482ec972c3e65a37b36db99a48f5b58e4869d
SUNMA4N <sunmaan22@gmail.com>
2026-09-02 15:34:32 +0900

    [Passed] 조건에 부합하는 수 (2ms, 0MB)

---
+++ trail1/조건에 부합하는 수/number-that-meets-the-condition.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a;
+    cin >> a;
+    
+    for(int i=1;i<=a;++i){
+        if((i%2==0&&i%4!=0)||((i/8)%2==0)||(i%7<4)){
+            continue;
+        }
+        cout << i << " ";
+    }
+    // Please write your code here.
+    return 0;
+}
