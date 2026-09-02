5d2006b5fc029ba80bc824c039f087039c622137
SUNMA4N <sunmaan22@gmail.com>
2026-09-02 09:47:47 +0900

    [Passed] 배수의 개수 (2ms, 0MB)

---
+++ trail1/배수의 개수/number-of-multipliers.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n, cnt1=0, cnt2=0;
+
+    for(int i=0;i<10;++i){
+        cin>>n;
+        if(n%3==0){
+            cnt1++;
+        }
+        if(n%5==0){
+            cnt2++;
+        }
+    }
+    cout <<cnt1<<" "<<cnt2;
+    // Please write your code here.
+    return 0;
+}
