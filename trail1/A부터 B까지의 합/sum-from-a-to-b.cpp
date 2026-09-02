cdad64a87e1940847ad72e7f5327742ec12d8657
SUNMA4N <sunmaan22@gmail.com>
2026-09-02 11:04:18 +0900

    [Passed] A부터 B까지의 합 (1ms, 0MB)

---
+++ trail1/A부터 B까지의 합/sum-from-a-to-b.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a,b,sum=0;
+    cin>>a>>b;
+
+    for(int i=a;i<=b;++i){
+        sum +=i;
+    }
+    cout << sum;
+    // Please write your code here.
+    return 0;
+}
