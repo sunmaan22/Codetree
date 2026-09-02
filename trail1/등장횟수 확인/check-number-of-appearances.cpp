e6ed17831ea86e0e8e2b3f2cd4f16f77cab035a4
SUNMA4N <sunmaan22@gmail.com>
2026-09-02 10:37:05 +0900

    [Passed] 등장횟수 확인 (1ms, 0MB)

---
+++ trail1/등장횟수 확인/check-number-of-appearances.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n,cnt=0;
+
+    for(int i=0;i<5;++i){
+        cin >> n;
+        if(n%2==0){
+            cnt++;
+        }
+    }
+    cout << cnt;
+    // Please write your code here.
+    return 0;
+}
