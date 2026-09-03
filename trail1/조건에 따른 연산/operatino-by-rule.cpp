9cb211e102be960a29e6a4075e152dfa1ea1ca5b
SUNMA4N <sunmaan22@gmail.com>
2026-09-03 09:46:21 +0900

    [Passed] 조건에 따른 연산 (2ms, 0MB)

---
+++ trail1/조건에 따른 연산/operatino-by-rule.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n,cnt=0;
+    cin >> n;
+
+    while(n<1000){
+        if(n%2==0){
+            n = n*3+1;
+            cnt++;
+        }
+        else{
+            n = n*2+2;
+            cnt++;
+        }
+    }
+    cout << cnt;
+    
+    // Please write your code here.
+    return 0;
+}
