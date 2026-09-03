de43406323a2b6ecd1a2fbdce64e7ada0c2c3ad3
SUNMA4N <sunmaan22@gmail.com>
2026-09-03 09:25:12 +0900

    [Passed] 몫 출력하기 (2ms, 0MB)

---
+++ trail1/몫 출력하기/print-share.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n,cnt=0;
+    while(cnt<3){
+        cin >> n;
+        if(n%2==0){
+            cout << n/2 << "\n";
+            cnt++;
+
+        }
+
+    }
+    // Please write your code here.
+    return 0;
+}
