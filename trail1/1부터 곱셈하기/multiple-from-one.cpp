393a470aec08880ae0936d092cd3b0254e2a3989
SUNMA4N <sunmaan22@gmail.com>
2026-09-03 09:03:37 +0900

    [Passed] 1부터 곱셈하기 (2ms, 0MB)

---
+++ trail1/1부터 곱셈하기/multiple-from-one.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n,prod=1;
+    cin >> n;
+
+    for(int i=1;i<=10;++i){
+        prod *= i;
+        if(prod>=n){
+            cout << i;
+            break;
+        }
+    }
+    // Please write your code here.
+    return 0;
+}
