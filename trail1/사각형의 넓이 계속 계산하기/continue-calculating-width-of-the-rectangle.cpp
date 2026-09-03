a7a13932e92d07e274ce7f4e3ccf8805e93807ea
SUNMA4N <sunmaan22@gmail.com>
2026-09-03 09:27:30 +0900

    [Passed] 사각형의 넓이 계속 계산하기 (2ms, 0MB)

---
+++ trail1/사각형의 넓이 계속 계산하기/continue-calculating-width-of-the-rectangle.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    char a;
+    int x,y;
+
+    while(1){
+        cin >> x >> y >> a;
+        cout << x*y << "\n";
+        if(a=='C'){
+            break;
+        }
+        
+    }
+    // Please write your code here.
+    return 0;
+}
