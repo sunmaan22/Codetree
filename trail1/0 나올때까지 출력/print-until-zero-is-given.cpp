5aa9f3da428953a5382c7acc158db1ee63994391
SUNMA4N <sunmaan22@gmail.com>
2026-09-03 09:14:34 +0900

    [Passed] 0 나올때까지 출력 (2ms, 0MB)

---
+++ trail1/0 나올때까지 출력/print-until-zero-is-given.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    while(1){
+        cin >> n;
+        if(n==0){
+            break;
+        }
+        cout << n<<"\n";
+    }
+    // Please write your code here.
+    return 0;
+}
