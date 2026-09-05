e993c75df83dbf7c70a5b4a7e0f2bb7b11982f7d
SUNMA4N <sunmaan22@gmail.com>
2026-09-05 16:53:28 +0900

    [Passed] 원소 10개의 합 (2ms, 0MB)

---
+++ trail1/원소 10개의 합/sum-of-10-elements.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int sum=0, arr[10];
+    for(int i=0;i<10;++i){
+        cin >> arr[i];
+        sum +=arr[i];
+    }
+    cout << sum;
+
+    // Please write your code here.
+    return 0;
+}
